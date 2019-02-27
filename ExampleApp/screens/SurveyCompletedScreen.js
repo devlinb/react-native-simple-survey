99f970eaf1daa697654d7aee5b74af3c79d7e80c
Devlin Bentley <devlin.bentley@gmail.com>
2019-02-26 17:08:11 -0800

    First check-in

---
+++ ExampleApp/screens/SurveyCompletedScreen.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, Button, Text, TouchableOpacity, View } from 'react-native';
+
+export default class SurveyCompletedScreen extends Component {
+    render() {
+        return <Text>Survey Finished!</Text>
+    }
+}
---
+++ ExampleApp/screens/SurveyScreen.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, Button, Text, TouchableOpacity, View } from 'react-native';
+
+export default class SurveyScreen extends Component {
+    render() {
+        return (<Text>Starting the survey!</Text>);
+    }
+}
