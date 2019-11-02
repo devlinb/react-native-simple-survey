71240e233df275f09b48516b4074dc8148bbb739
Devlin Bentley <devlin.bentley@gmail.com>
2019-11-01 18:52:00 -0700

    Added support for default values, deselecting items, auto advancing individual questions

--- ExampleApp/screens/SurveyCompletedScreen.js
+++ ExampleApp/screens/SurveyCompletedScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 const GREEN = 'rgba(141,196,63,1)';
 const PURPLE = 'rgba(108,48,237,1)';
@@ -26,12 +26,20 @@ export default class SurveyCompletedScreen extends Component {
         return (
             <View style={styles.background}>
                 <View style={styles.container}>
-                    <Text style={styles.questionText}>The results are in!</Text>
-                    <Text style={styles.questionText}>Your favorite color: {answers.favoriteColor}</Text>
-                    <Text style={styles.questionText}>Your favorite number: {answers.favoriteNumber}</Text>
-                    <Text style={styles.questionText}>Your favorite pet: {answers.favoritePet.value}</Text>
-                    <Text style={styles.questionText}>Your favorite foods: {answers.favoriteFoods[0].value} and {answers.favoriteFoods[1].value}</Text>
-                    <Text>Raw JSON: {JSON.stringify(this.props.navigation.getParam('surveyAnswers', {}))}</Text>
+                    <ScrollView>
+                        <Text style={styles.questionText}>The results are in!</Text>
+                        <Text style={styles.questionText}>Your favorite color: {answers.favoriteColor}</Text>
+                        <Text style={styles.questionText}>Your favorite number: {answers.favoriteNumber}</Text>
+                        <Text style={styles.questionText}>Your favorite pet: {answers.favoritePet.value}</Text>
+                        <Text style={styles.questionText}>Your favorite foods: {answers.favoriteFoods[0].value} and {answers.favoriteFoods[1].value}</Text>
+                        <Text style={styles.questionText}>How you relax: {answers.relax[0].value} and {answers.relax[1].value}</Text>
+                        <Text style={styles.questionText}>When confronted with a radio button you picked: {answers.radio.value}</Text>
+                        <Text style={styles.questionText}>When given a default you chose: the {answers.singleDefault.value}</Text>
+                        <Text style={styles.questionText}>When given a multiple defaults you chose: the {answers.multipleDefaults[0].value} and the {answers.multipleDefaults[1].value}</Text>
+
+
+                        <Text>Raw JSON: {JSON.stringify(this.props.navigation.getParam('surveyAnswers', {}))}</Text>
+                    </ScrollView>
                 </View>
             </View>
         );
@@ -53,6 +61,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         elevation: 20,
         borderRadius: 10,
+        maxHeight: '80%',
     },
     questionText: {
         marginBottom: 20,
